bf8dcfe330db61dac89f9562f6a78a6afc31bc69
Robert Clukey <robertclukey1234@gmail.com>
2025-12-02 12:36:20 -0600

    Update 3512-Minimum-Operations-to-Make-Array-Sum-Divisible-by-K.cpp

--- 3512-Minimum-Operations-to-Make-Array-Sum-Divisible-by-K.cpp
+++ 3512-Minimum-Operations-to-Make-Array-Sum-Divisible-by-K.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         int count = 0;
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            count += nums[i];
-        }
+        for (int i = 0; i < nums.size(); ++i) count += nums[i];
         return count % k;
     }
 };
