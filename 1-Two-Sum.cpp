65bddabc5d880c4c2ed8e68157878652d8dd4f38
Robert Clukey <robertclukey1234@gmail.com>
2025-12-02 12:40:25 -0600

    Update 1-Two-Sum.cpp

--- 1-Two-Sum.cpp
+++ 1-Two-Sum.cpp
@@ -3,13 +3,12 @@ public:
     vector<int> twoSum(vector<int> &nums, int target) {
         unordered_map<int, int> hash;
         for (int i = 0; i < nums.size(); ++i) {
-            int comp = target - nums[i];                // Determine the complement of each number in the vector
-            if (hash.find(comp) != hash.end()) {        // If the complement is in the hash table
+            int comp = target - nums[i];
+            if (hash.find(comp) != hash.end()) {
                 return {hash[comp], i};
             }
-            hash[nums[i]] = i;                          // Add hash to the table
+            hash[nums[i]] = i;
         }
-        // Return an empty vector if no solution is found
         return {};
     }
 };
