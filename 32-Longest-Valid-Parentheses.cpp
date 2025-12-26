8a646297c3f1083933d21eaa46dd563c4c7f8b1c
Robert Clukey <robertclukey1234@gmail.com>
2025-12-26 15:07:57 -0600

    Create 32-Longest-Valid-Parentheses.cpp

---
+++ 32-Longest-Valid-Parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        if (n <= 1) return 0;
+        stack<int> st;
+        st.push(-1);
+        int maximum = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (s[i] == '(') st.push(i);
+            else
+            {
+                st.pop();
+                if (st.empty()) st.push(i);
+                else maximum = max(maximum, i-st.top());
+            }
+        }
+        return maximum;
+    }
+};
