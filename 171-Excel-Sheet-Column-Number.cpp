7f5cb7599fc4e82ee2980520d7c87b4bffaeacd5
Robert Clukey <robertclukey1234@gmail.com>
2026-01-02 16:02:56 -0600

    Delete 171-Excel-Sheet-Column-Number.cpp

--- 171-Excel-Sheet-Column-Number.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int titleToNumber(string columnTitle) {
-        int out = 0;
-        for (char c : columnTitle) out = (out*26) + (c-'@');
-        return out;
-    }
-};
