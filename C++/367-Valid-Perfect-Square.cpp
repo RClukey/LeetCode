7979e10df27481bc2498d72f914161f6bcc6667c
Robert Clukey <robertclukey1234@gmail.com>
2026-01-06 22:44:27 -0600

    Create 367-Valid-Perfect-Square.cpp

---
+++ C++/367-Valid-Perfect-Square.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num == 1) return true;
+        
+        int left = 2, right = num/2;
+        while (left <= right)
+        {
+            long long mid = (right+left)/2;
+            if (mid*mid == num) return true;
+            if (mid*mid > num) right = mid-1;
+            else left = mid+1;
+        }
+        return false;
+    }
+};
