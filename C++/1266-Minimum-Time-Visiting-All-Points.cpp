b5960127bff14cefa7f840da63b8a597fad5acca
Robert Clukey <robertclukey1234@gmail.com>
2026-01-12 09:39:36 -0600

    Create 1266-Minimum-Time-Visiting-All-Points.cpp

---
+++ C++/1266-Minimum-Time-Visiting-All-Points.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        unsigned int out = 0;
+        for (int i = 0; i < points.size()-1; i++) out += max(abs(points[i][0] - points[i+1][0]), abs(points[i][1]- points[i+1][1]));
+        return out;
+    }
+};
